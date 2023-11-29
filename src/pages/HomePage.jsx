68859523abff2ed5d2fa548fd6cbad1cf75692bf
Ana URL <ana.urlic@gmail.com>
2023-11-29 12:38:42 +0100

    add homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "../index.css";
+import homeImage from "../assets/img/homeImage.svg"
+
+
+const HomePage = () => {
+  return (
+    <div className="container record">
+      <h1> Hey, Alexandra </h1>
+      <p> What&apos;s the health vibe for the day?</p>
+      <div className="button-container">
+      <Link to="/record" className="button-link"> Track </Link>
+      <Link to="/health" className="button-link">Health Records </Link>
+      </div>
+      <img src={homeImage} alt="Illustration" />
+    
+    </div>
+  );
+};
+
+export default HomePage;
